841711bb01c4bc6b2d4cce63f1ab217d4476bc57
CharlotteBrodie <charbrodie@sgpnyc.com>
2026-07-20 10:01:03 -0400

    Updates - created a solutions file to run the app

---
+++ sgpnyc_Website.Server/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
